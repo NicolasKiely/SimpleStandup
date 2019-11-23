412402ef1c1c8b7ac8ceca39f858f59af3f1e6c0
Nicolas Kiely <mitchell3141@gmail.com>
2019-11-23 14:02:44 -0500

    Issue #1: Login form
    Initial login form on index page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import StandupIndex from "./components/standup-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">SimpleStandup</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/" exact component={StandupIndex} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
