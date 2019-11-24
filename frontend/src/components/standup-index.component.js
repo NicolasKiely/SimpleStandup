a3a583800812d528c3c1424568cd7ea2834a0217
Nicolas Kiely <mitchell3141@gmail.com>
2019-11-24 00:11:02 -0500

    Issues #1, #2: Login/Register form
    Select between login and register form

--- frontend/src/components/standup-index.component.js
+++ frontend/src/components/standup-index.component.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {LoginForm, RegisterForm} from "./standup-login.component"
+import LoginRegisterForm from "./standup-login.component"
 
 
 export default class StandupIndex extends Component {
@@ -16,25 +16,7 @@ export default class StandupIndex extends Component {
       <div style={{marginTop: 10}}>
         <div className="row">
           <div className="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
-            <div className="login-register-form">
-              <div className="login-register-header">
-                <span className="login-form-title login-register-form-title login-register-title-active"
-                >
-                  Login
-                </span>
-                <span className="register-form-title login-register-form-title"
-                >
-                  Register
-                </span>
-              </div>
-
-              <div className="login-register-body">
-                <LoginForm />
-              </div>
-              <div className="login-register-body">
-                <RegisterForm />
-              </div>
-            </div>
+            <LoginRegisterForm />
           </div>
         </div>
       </div>
--- frontend/src/components/standup-login.component.js
+++ frontend/src/components/standup-login.component.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 
+/**
+ * Component for just login form
+ */
 class LoginForm extends Component {
   constructor(props) {
     super(props);
@@ -17,25 +20,16 @@ class LoginForm extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-
     console.log('Submitting login');
-
-    this.setState({
-      email: '',
-      pass: ''
-    });
+    this.setState({email: '', pass: ''});
   }
 
   onChangeEmail(e) {
-    this.setState({
-      email: e.target.value
-    });
+    this.setState({email: e.target.value});
   }
 
   onChangePass(e){
-    this.setState({
-      pass: e.target.value,
-    });
+    this.setState({pass: e.target.value});
   }
 
   render(){
@@ -68,53 +62,36 @@ class LoginForm extends Component {
 }
 
 
+/**
+ * Component for just register form
+ */
 class RegisterForm extends Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      email: '',
-      pass: '',
-      fname: '',
-      lname: ''
-    };
+    this.state = {email: '', pass: '', fname: '', lname: ''};
   }
 
   onSubmitRegister(e) {
     e.preventDefault();
-
-    this.setState({
-      email: '',
-      pass: '',
-      fname: '',
-      lname: ''
-    });
-
+    this.setState({email: '', pass: '', fname: '', lname: ''});
     console.log('Submitting register');
   }
 
   onChangeEmail(e){
-    this.setState({
-      email: e.target.value
-    })
+    this.setState({email: e.target.value})
   }
 
   onChangePass(e){
-    this.setState({
-      pass: e.target.value
-    })
+    this.setState({pass: e.target.value})
   }
 
   onChangeFName(e){
-    this.setState({
-      fname: e.target.value
-    });
+    this.setState({fname: e.target.value});
   }
 
   onChangeLName(e){
-    this.setState({
-      lname: e.target.value
-    })
+    this.setState({lname: e.target.value})
   }
 
   render() {
@@ -127,25 +104,32 @@ class RegisterForm extends Component {
                  onChange={this.onChangeEmail}
           />
         </div>
-        <div>
+        <div className="form-group">
           <label>Password</label>
           <input type="password"
                  className="form-control"
                  onChange={this.onChangePass}
           />
         </div>
-        <div>
+        <div className="form-group">
           <label>First Name</label>
           <input type="text"
                  className="form-control"
                  onChange={this.onChangeFName}
           />
         </div>
-        <div>
+        <div className="form-group">
           <label>Last Name</label>
           <input type="text"
                  className="form-control"
                  onChange={this.onChangeLName}
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <input type="submit"
+                 value="Register"
+                 className="btn btn-primary btn-block"
           />
         </div>
       </form>
@@ -154,4 +138,77 @@ class RegisterForm extends Component {
 }
 
 
+/**
+ * Combined selectable login/registration form with header
+ */
+class LoginRegisterForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSelectLogin = this.onSelectLogin.bind(this);
+    this.onSelectRegister = this.onSelectRegister.bind(this);
+
+    this.state = {
+      login_form_active: true,
+      register_form_active: false
+    }
+  }
+
+  onSelectLogin() {
+    this.setState({
+      login_form_active: true,
+      register_form_active: false
+    })
+  }
+
+  onSelectRegister() {
+    this.setState({
+      login_form_active: false,
+      register_form_active: true
+    })
+  }
+
+  render() {
+    /* Form body toggling */
+    let login_form_style = this.state.login_form_active ? {} : {display: 'none'};
+    let register_form_style = this.state.register_form_active ? {} : {display: 'none'};
+
+    /* Form header toggling */
+    let login_header_class = this.state.login_form_active ?
+      "login-title-selector active-title-selector" :
+      "login-title-selector inactive-title-selector";
+    let register_header_class = this.state.register_form_active ?
+      "register-title-selector active-title-selector" :
+      "register-title-selector inactive-title-selector";
+
+    return (
+      <div className="login-register-form">
+        <div className="login-register-header">
+          <div className={login_header_class} onClick={this.onSelectLogin}>
+            <span className="login-form-title login-register-form-title login-register-title-active">
+              Login
+            </span>
+          </div>
+          <div className={register_header_class} onClick={this.onSelectRegister}>
+            <span className="register-form-title login-register-form-title"
+                  onClick={this.onSelectRegister}
+            >
+              Register
+            </span>
+          </div>
+        </div>
+
+        <div className="login-register-body" style={login_form_style}>
+          <LoginForm active="true" />
+        </div>
+        <div className="login-register-body" style={register_form_style}>
+          <RegisterForm />
+        </div>
+      </div>
+    );
+  }
+}
+
+
 export {LoginForm, RegisterForm};
+export default LoginRegisterForm;
