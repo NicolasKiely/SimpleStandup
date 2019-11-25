1411a2d227d7217ceb20ef4c77433c194587d110
Nicolas Kiely <mitchell3141@gmail.com>
2019-11-24 22:02:57 -0500

    Issue #4: User Registration
    Pass through internal error if applicable for user registration

--- backend/endpoints.ts
+++ backend/endpoints.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const ERR_NOT_IMPLEMENTED = 'EXT_NOT_IMPLEMENTED';
 const MSG_NOT_IMPLEMENTED = 'External endpoint not implemented';
 
@@ -8,12 +10,23 @@ function auth_login(request, results) {
   );
 }
 
-
 function auth_register(request, results) {
-  results.status(500).json(
-    {'payload': {}, 'error': ERR_NOT_IMPLEMENTED, 'message': MSG_NOT_IMPLEMENTED}
+  axios.post(
+    'http://localhost:8040/auth/user/register/',
+    {BACKEND_SECRET: ''}
+  ).then(
+    res => {
+      results.status(500).json(
+        {'payload': {}, 'error': ERR_NOT_IMPLEMENTED, 'message': MSG_NOT_IMPLEMENTED}
+      );
+    },
+    err => {
+      let ext_status = err.response.data.status || 500;
+      results.status(ext_status).json(
+        {'payload': {}, 'error': err.response.data.error, 'message': err.response.data.message}
+      );
+    }
   );
 }
-
 
 export {auth_login, auth_register};
