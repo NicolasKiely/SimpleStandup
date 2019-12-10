13789df0b2c8e73ad1b762c87babfeae4bc239db
Nicolas Kiely <mitchell3141@gmail.com>
2019-12-10 17:49:13 -0500

    Issue #3: User Login
    Update

--- frontend/src/components/standup-index.component.js
+++ frontend/src/components/standup-index.component.js
@@ -13,13 +13,16 @@ export default class StandupIndex extends Component {
     this.onLogin = function(user_email, token){
       console.log('Login callback called for user: ' + user_email);
       console.log('Login token: ' + token);
-    }
+      this.setState({logged_in: true});
+    };
+    this.onLogin = this.onLogin.bind(this);
   }
 
   render() {
+    const display_style = this.state.logged_in ? {display: 'none'} : {};
     return (
       <div style={{marginTop: 10}}>
-        <div className="row">
+        <div className="row" style={display_style}>
           <div className="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
             <LoginRegisterForm onLogin={this.onLogin} />
           </div>
