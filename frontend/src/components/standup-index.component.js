c74516fe99db1e89dd784b1a7380c9d35c64cc1b
Nicolas Kiely <mitchell3141@gmail.com>
2020-01-20 22:03:36 -0500

    Issue #8: Persist login on frontend

--- frontend/src/components/standup-index.component.js
+++ frontend/src/components/standup-index.component.js
@@ -6,28 +6,51 @@ export default class StandupIndex extends Component {
   constructor(props) {
     super(props);
 
+    let first_token = localStorage.getItem("login_token");
+    let first_logged_in;
+    if (first_token){
+      console.log("Logged in with token " + first_token);
+      first_logged_in = true;
+    } else {
+      console.log("Not logged in yet");
+      first_logged_in = false;
+    }
+
     this.state = {
-      logged_in: false,
+      logged_in: first_logged_in,
     };
 
     this.onLogin = function(user_email, token){
       console.log('Login callback called for user: ' + user_email);
       console.log('Login token: ' + token);
       this.setState({logged_in: true});
+      localStorage.setItem("login_token", token);
     };
     this.onLogin = this.onLogin.bind(this);
   }
 
   render() {
-    const display_style = this.state.logged_in ? {display: 'none'} : {};
-    return (
-      <div style={{marginTop: 10}}>
-        <div className="row" style={display_style}>
-          <div className="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
-            <LoginRegisterForm onLogin={this.onLogin} />
+    if (this.state.logged_in){
+      return (
+        <div style={{marginTop: 10}}>
+          <div className="row">
+            <div className="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
+
+            </div>
           </div>
         </div>
-      </div>
-    );
+      )
+
+    } else {
+      return (
+        <div style={{marginTop: 10}}>
+          <div className="row">
+            <div className="col-sm-12 col-md-8 col-lg-6 offset-md-2 offset-lg-3">
+              <LoginRegisterForm onLogin={this.onLogin} />
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
