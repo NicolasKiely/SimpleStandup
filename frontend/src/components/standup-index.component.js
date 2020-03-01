c5aa0ddfc0825dcfe99d5863f9da4aac0060fb4c
Nicolas Kiely <mitchell3141@gmail.com>
2020-03-01 18:59:15 -0500

    Issue #18: Show error message if creating channel failed

--- frontend/src/components/standup-index.component.js
+++ frontend/src/components/standup-index.component.js
@@ -10,7 +10,8 @@ export default class StandupIndex extends Component {
       display_new_channel_form: false,
       user_email: props.user_email,
       user_token: props.user_token,
-      channel_name: ""
+      channel_name: "",
+      error_msg: ""
     };
 
     /* Click handler for opening/closing form for new channels */
@@ -33,9 +34,12 @@ export default class StandupIndex extends Component {
       backend_request("/api/1/channels", props.global_handler, "PUT", send_data).then(
         res => {
           console.log("Created new channel!");
+          this.setState({error_msg: ""});
         },
         err => {
-          console.log("Failed to create channel");
+          const error_msg = err.response && err.response.data ?
+            err.response.data.message: "Failed to create channel";
+          this.setState({error_msg: error_msg});
         }
       )
     };
@@ -57,6 +61,20 @@ export default class StandupIndex extends Component {
       "Cancel" : "Create New Channel";
     const new_channel_button_class = this.state.display_new_channel_form ?
       "btn btn-block btn-outline-secondary" : "btn btn-block btn-secondary";
+    const error_msg = this.state.error_msg;
+
+    let error_div = <div/>;
+    if (error_msg){
+      error_div =(
+        <div className="row">
+          <div className="col-12">
+            <div className="alert alert-danger">
+              {error_msg}
+            </div>
+          </div>
+        </div>
+      )
+    }
 
     /* Form elements for creating new channel */
     const new_channel_form = <div>
@@ -85,6 +103,8 @@ export default class StandupIndex extends Component {
     return (
       <div style={{marginTop: 10}}>
         <div className="channel-list-form-container">
+          {error_div}
+
           {main_body}
 
           {new_channel_form}
