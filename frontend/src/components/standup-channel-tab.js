365325603dd60446e8c2d6f119b8b6bc75223c6a
Nicolas Kiely <mitchell3141@gmail.com>
2020-03-07 19:32:10 -0500

    Issue #20: Channel index UI
    Expand channel tab on click

--- frontend/src/components/standup-channel-tab.js
+++ frontend/src/components/standup-channel-tab.js
@@ -10,20 +10,65 @@ export default class ChannelIndexTab extends Component {
     this.state = {
       channel_name: props.channel_name,
       channel_owner: props.channel_owner,
-      user_email: props.user_email
+      user_email: props.user_email,
+      expanded: false
     };
+
+    /* Callback for tab being clicked */
+    this.onClick = function(){
+      this.setState({expanded: !this.state.expanded});
+    };
+    this.onClick = this.onClick.bind(this);
+
+    /* Callback for archiving tab */
+    this.onArchive = function(){
+      return true;
+    };
+    this.onArchive = this.onArchive.bind(this);
   }
 
   render(){
-    const is_owner = this.state.channel_owner === this.state.user_email;
-    const channel_owner_span = (
+    const isOwner = this.state.channel_owner === this.state.user_email;
+    const expanded = this.state.expanded;
+    const wrapperClass = expanded ?
+      "channel-tab-wrapper channel-tab-wrapper-expanded" :
+      "channel-tab-wrapper channel-tab-wrapper-collapsed"
+    ;
+    const channelOwnerSpan = isOwner ?
+      null :
       <span className="channel-tab-owner"> - {this.state.channel_owner}</span>
-    );
+    ;
+    let channelTabDetails;
+    if (expanded){
+      /* Showing expanded details */
+      const archiveBtn = (
+        <div className="col-1 offset-10">
+          <button className="btn btn-outline-secondary btn-sm">
+            Archive
+          </button>
+        </div>
+      );
+      channelTabDetails = (
+        <div className="row">
+          <div className="col-sm-12">
+            {archiveBtn}
+          </div>
+        </div>
+      );
+    } else {
+      /* Tab is collapsed */
+      channelTabDetails = null;
+    }
 
     return (
-      <div className="channel-tab-wrapper">
-        <span className="channel-tab-title">Channel: {this.state.channel_name}</span>
-        {is_owner ? "" :  channel_owner_span}
+      <div className={wrapperClass} onClick={this.onClick}>
+        <div className="row">
+          <div className="col-sm-12">
+            <span className="channel-tab-title">Channel: {this.state.channel_name}</span>
+            {channelOwnerSpan}
+          </div>
+        </div>
+        {channelTabDetails}
       </div>
     );
   }
